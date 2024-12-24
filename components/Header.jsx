defc0fb9e3e75aa5c09727cd834e69d5f5ce9775
George <killyen444@gmail.com>
2024-12-24 17:49:12 +0800

    feat: added hamburgur menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,41 +1,54 @@
 "use client";
-import { useState } from 'react';
-import { notoSansTCClass } from '../app/layout.js';
-import CustomButton from './CustomButton.jsx';
-import { useNavigation } from '@/lib/functions.js';
-import Link from 'next/link';
-import './css/Header.css';
-
-
+import { useState, useEffect } from "react";
+import { notoSansTCClass } from "../app/layout.js";
+import CustomButton from "./CustomButton.jsx";
+import { useNavigation } from "@/lib/functions.js";
+import Link from "next/link";
+import "./css/Header.css";
 
 const Header = () => {
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
+        setIsMenuOpen((prev) => !prev);
+    };
 
     const navigate = useNavigation();
 
     const handleUserLogin = () => {
         alert("user login");
-        navigate('/login');
-    }
+        navigate("/login");
+    };
 
     const handleUserRegister = () => {
         alert("user register");
-        navigate('/register');
-    }
+        navigate("/register");
+    };
 
+    // when the window is resized, close the menu if the window is wider than 768px
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) {
+                setIsMenuOpen(false); 
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     return (
         <header className="header">
             <div className={`header-logo ${notoSansTCClass}`}>LOGO</div>
-            <button className="hamburger-menu" onClick={toggleMenu}>
+            <button
+                className="hamburger-menu"
+                onClick={toggleMenu}
+                aria-label="Toggle navigation menu" 
+                aria-expanded={isMenuOpen} // 用於輔助技術 
+            >
                 ☰
             </button>
-            <nav className={`header-nav ${isMenuOpen ? 'open' : ''} ${notoSansTCClass}`}>
+            <nav className={`header-nav ${isMenuOpen ? "open" : ""} ${notoSansTCClass}`}>
                 <div className="header-nav-options">
                     <Link href="#">繪師</Link>
                     <Link href="#">市集</Link>
@@ -48,12 +61,8 @@ const Header = () => {
                     <CustomButton title="註冊" onClick={handleUserRegister} />
                 </div>
             </nav>
-            <button className="hamburger-menu" onClick={toggleMenu}>
-                {/* 用 Unicode 或圖標 */}
-
-            </button>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
