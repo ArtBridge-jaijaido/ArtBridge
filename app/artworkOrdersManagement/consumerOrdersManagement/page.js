e7805bb9045bfc423c9c46b92a55d4a802118d76
RichardBoyz <r1382333@gmail.com>
2025-06-10 11:45:03 +0800

    style: format consumerOrdersManagement page

--- app/artworkOrdersManagement/consumerOrdersManagement/page.js
+++ app/artworkOrdersManagement/consumerOrdersManagement/page.js
@@ -1,74 +1,62 @@
 "use client";
-import {useState, useEffect } from "react";
-import { notoSansTCClass } from '@/app/layout.js';
-import ArtworkOrderManagementTabs from '@/components/Tabs/ArtworkOrderManagementTab';
+import { notoSansTCClass } from "@/app/layout.js";
 import ArtworkOrderCard from "@/components/ArtworkOrderCard/ArtworkOrderCard.jsx";
-import "./consumerOrdersManagement.css";
+import ArtworkOrderManagementTabs from "@/components/Tabs/ArtworkOrderManagementTab";
 import { useSelector } from "react-redux";
-
+import "./consumerOrdersManagement.css";
 
 const ConsumerOrdersManagementPage = () => {
-  
-  
-    const consumerOrders = useSelector((state) => state.artworkOrder.consumerOrders);
+  const consumerOrders = useSelector(
+    (state) => state.artworkOrder.consumerOrders
+  );
 
-   
-   
-    const tabs = [
-        {
-            label: "目前案件",
-            content: (
-              <div className="consumerOrdersManagement-currentCase-container">
-                {[...consumerOrders]
-                  .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
-                  .map((order, index) => (
-                    <ArtworkOrderCard
-                      key={order.artworkOrderId}
-                      orderIndex={index + 1}
-                      statusLabel={order.status}
-                      OrderTitle={order.marketName}
-                      OrderSource={order.orderSource}
-                      OrderEndDate={order.endDate}
-                      OrderMilestone={order.artworkOrderMilestones}
-                      OrderAssignedPainter={order.assignedPainterUid}
-                      OrderEntruster={order.userUid}
-                      orderId={order.artworkOrderId}
-                      exampleImageUrl={order.exampleImageUrl || ""}
-                      referenceImageUrl={order.referenceImageUrl || ""}
-                      customRequirement={order.customRequirement || ""}
-                    />
-                  ))}
-              </div>
-            ),
-          },
-        {
-            label: "歷史案件",
-            content: <div>Consumer歷史案件內容</div>,
-        },
-        {
-            label: "行事曆",
-            content: <div>Consumer行事曆內容</div>,
-        },
-        {
-            label: "查看詳細資料",
-            content: <div>Consumer查看詳細資料內容</div>,
-        }
-    ]
-
-
-
-
-
-    return (
-        <div className={`consumerOrdersManagementPage ${notoSansTCClass}`} >
-            <div className="consumerOrdersManagement-tab-container">
-                < ArtworkOrderManagementTabs tabs={tabs} />
-            </div>
+  const tabs = [
+    {
+      label: "目前案件",
+      content: (
+        <div className="consumerOrdersManagement-currentCase-container">
+          {[...consumerOrders]
+            .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+            .map((order, index) => (
+              <ArtworkOrderCard
+                key={order.artworkOrderId}
+                orderIndex={index + 1}
+                statusLabel={order.status}
+                OrderTitle={order.marketName}
+                OrderSource={order.orderSource}
+                OrderEndDate={order.endDate}
+                OrderAssignedPainter={order.assignedPainterUid}
+                OrderEntruster={order.userUid}
+                orderId={order.artworkOrderId}
+                exampleImageUrl={order.exampleImageUrl || ""}
+                referenceImageUrl={order.referenceImageUrl || ""}
+                customRequirement={order.customRequirement || ""}
+              />
+            ))}
         </div>
-    )
+      ),
+    },
+    {
+      label: "歷史案件",
+      content: <div>Consumer歷史案件內容</div>,
+    },
+    {
+      label: "行事曆",
+      content: <div>Consumer行事曆內容</div>,
+    },
+    {
+      label: "查看詳細資料",
+      content: <div>Consumer查看詳細資料內容</div>,
+    },
+  ];
 
-
-}
-
+  return (
+    <div className={`consumerOrdersManagementPage ${notoSansTCClass}`}>
+      <div className="consumerOrdersManagement-tab-container">
+        <ArtworkOrderManagementTabs tabs={tabs} />
+      </div>
+    </div>
+  );
+};
 
 export default ConsumerOrdersManagementPage;
