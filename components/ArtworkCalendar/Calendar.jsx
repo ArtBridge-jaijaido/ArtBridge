e851bcf6ca176e87bb60b4524a1c418f955aa860
RichardBoyz <r1382333@gmail.com>
2025-06-11 09:02:38 +0800

    feat(ArtworkCalendar): set calendar header cell

--- components/ArtworkCalendar/Calendar.jsx
+++ components/ArtworkCalendar/Calendar.jsx
@@ -1,6 +1,17 @@
 "use client";
 import "./Calendar.css";
+import { WEEKDAYS } from "./constants";
 
 export default function Calendar({ events, date }) {
-  return <div className="Calendar-table"></div>;
+  return (
+    <div className="Calendar-table">
+      <div className="Calendar-row">
+        {WEEKDAYS.map((label) => (
+          <div key={label} className="Calendar-header-cell">
+            {label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
