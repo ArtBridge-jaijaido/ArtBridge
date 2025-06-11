897e327c415106c8a0df156d9ba9c1d73413043d
RichardBoyz <r1382333@gmail.com>
2025-06-11 09:19:10 +0800

    feat(ArtworkCalendar): add draft components for calendar view

--- components/ArtworkCalendar/ArtworkCalendar.jsx
+++ components/ArtworkCalendar/ArtworkCalendar.jsx
@@ -1,9 +1,22 @@
+import { addMonths } from "date-fns";
+import { useState } from "react";
 import "./ArtworkCalendar.css";
+import Calendar from "./Calendar";
+import CalendarHeader from "./CalendarHeader";
 
 export const ArtworkCalendar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
   return (
     <div className="ArtworkCalendar-container">
-      <div className="ArtworkCalendar-calendar-field"></div>
+      <div className="ArtworkCalendar-calendar-field">
+        <CalendarHeader
+          currentDate={currentDate}
+          onPrev={() => setCurrentDate((prev) => addMonths(prev, -1))}
+          onNext={() => setCurrentDate((prev) => addMonths(prev, 1))}
+        />
+        <Calendar events={[]} date={currentDate} />
+      </div>
     </div>
   );
 };
