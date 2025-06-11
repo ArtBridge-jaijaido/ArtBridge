c3287ddb65180532f354ed8d992074002f59f8f7
RichardBoyz <r1382333@gmail.com>
2025-06-11 09:16:45 +0800

    feat(ArtworkCalendar): add ArtworkCalendar component

---
+++ components/ArtworkCalendar/ArtworkCalendar.jsx
@@ -0,0 +1,9 @@
+import "./ArtworkCalendar.css";
+
+export const ArtworkCalendar = () => {
+  return (
+    <div className="ArtworkCalendar-container">
+      <div className="ArtworkCalendar-calendar-field"></div>
+    </div>
+  );
+};
