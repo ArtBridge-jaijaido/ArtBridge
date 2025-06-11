aca9fcba124f578fca26af014964266fa11e8225
RichardBoyz <r1382333@gmail.com>
2025-06-11 09:14:59 +0800

    feat(ArtworkCalendar): Complete calendar contents ui

--- components/ArtworkCalendar/Calendar.jsx
+++ components/ArtworkCalendar/Calendar.jsx
@@ -1,8 +1,12 @@
 "use client";
+import { format, isSameDay, isSameMonth } from "date-fns";
 import "./Calendar.css";
-import { WEEKDAYS } from "./constants";
+import { DAYS_IN_WEEK, WEEKDAYS } from "./constants";
+import getCalendarGrid from "./getCalendarGrid";
 
 export default function Calendar({ events, date }) {
+  const days = getCalendarGrid(date);
+
   return (
     <div className="Calendar-table">
       <div className="Calendar-row">
@@ -12,6 +16,39 @@ export default function Calendar({ events, date }) {
           </div>
         ))}
       </div>
+
+      {Array.from({ length: Math.ceil(days.length / DAYS_IN_WEEK) }).map(
+        (_, rowIndex) => (
+          <div key={rowIndex} className="Calendar-row">
+            {days
+              .slice(
+                rowIndex * DAYS_IN_WEEK,
+                rowIndex * DAYS_IN_WEEK + DAYS_IN_WEEK
+              )
+              .map((day) => (
+                <div key={day.toString()} className="Calendar-cell">
+                  <div
+                    className={`Calendar-date ${
+                      isSameMonth(day, date)
+                        ? "Calendar-text"
+                        : "Calendar-text--other"
+                    }`}
+                  >
+                    {format(day, "d")}
+                  </div>
+                  <div className="Calendar-event-row">
+                    {events
+                      .filter((e) => isSameDay(day, new Date(e.date)))
+                      .map((e, i) => (
+                        <span key={i} />
+                        // TODO: 案件圓點
+                      ))}
+                  </div>
+                </div>
+              ))}
+          </div>
+        )
+      )}
     </div>
   );
 }
--- components/ArtworkCalendar/constants.js
+++ components/ArtworkCalendar/constants.js
@@ -1,1 +1,2 @@
 export const WEEKDAYS = ["日", "一", "二", "三", "四", "五", "六"];
+export const DAYS_IN_WEEK = 7;
