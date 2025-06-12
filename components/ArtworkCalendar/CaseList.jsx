a78a6aa09eb7da5b936a5a93764d82c5ac041159
RichardBoyz <r1382333@gmail.com>
2025-06-12 09:36:19 +0800

    feat(ArtworkCalendar): add prototype of CaseList

--- components/ArtworkCalendar/CaseList.jsx
+++ components/ArtworkCalendar/CaseList.jsx
@@ -1,5 +1,19 @@
 import "./CaseList.css";
+import CaseListItem from "./CaseListItem";
 
-export const CaseList = () => {
-  return <div>CaseList</div>;
+export const CaseList = ({ events }) => {
+  return (
+    <div className="CaseList-wrapper">
+      <div className="CaseList">
+        {events.map((event, index) => (
+          <CaseListItem
+            key={index}
+            color={event.color}
+            dateRange={event.dateRange}
+            title={event.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
